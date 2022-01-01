1de8868ae8b2da627bff52d80b9dc914cef1ca57
IslamGoher <sam.m.goher@gmail.com>
2022-01-01 19:59:01 +0200

    Add Filter functionality
    - Add filter by source, tag, perfectSolution

--- src/controllers/solution.ts
+++ src/controllers/solution.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from "express";
 import { solutionsAnonymousQueries } from "../queries/api-queries";
 import { pool } from "../database/pool";
 import { getSortingQuery } from "../utils/sort-database";
+import { getFilteringQueries } from "../utils/filter-database";
+import { ErrorResponse } from "../utils/error-response";
 
 // @route   GET '/api/v1/solutions/:email'
 // @desc    list all solutions for Anonymous user
@@ -14,14 +16,32 @@ export const getAllSolutionsAnonymous = async (
   try {
 
     const email = req.params.email;
+    const sortbydate = `${req.query.sortbydate}`;
+    const source = `${req.query.source}`;
+    const tag = `${req.query.tag}`;
+    const perfectSolution = `${req.query.perfectsolution}`;
 
     // sort solutions by date
-    let sortBy = getSortingQuery(`${req.query.sortbydate}`);
+    const sortBy = getSortingQuery(sortbydate);
 
+    // filter by 'source', 'tag', 'perfect_solution'
+    const {
+      filterBySource,
+      filterByTag,
+      filterByPerfectSolution 
+    } = getFilteringQueries(source, tag, perfectSolution);
+    
     const solutionsData = await pool.query(
-      `${solutionsAnonymousQueries} ${sortBy};`,
+      `${solutionsAnonymousQueries}
+      ${filterBySource}
+      ${filterByTag}
+      ${filterByPerfectSolution!}
+      ${sortBy};`,
       [email]
     );
+
+    if(solutionsData.rowCount === 0)
+      return next(new ErrorResponse(404, "solutions not found"));
 
     res.json(solutionsData.rows);
 
