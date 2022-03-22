6f50671af9d177c453adda32c63928029afdfcbc
IslamGoher <islam.m.goher@gmail.com>
2022-03-22 21:50:09 +0200

    Add validation for api 'get all solutions for anonymous user'

--- src/routes/solution.ts
+++ src/routes/solution.ts
@@ -1,9 +1,14 @@
 import express, { Router } from "express";
 import { getAllSolutionsAnonymous } from "../controllers/solution";
+import { validateGetAllSolutionsAnonymous } from "../middlewares/validation/solution-validation";
 
 export const router: Router = express.Router();
 
 // @route   GET '/api/v1/solutions/:email'
 // @desc    list all solutions for Anonymous user
 // @access  public
-router.get("/api/v1/solutions/:email", getAllSolutionsAnonymous);
+router.get(
+  "/api/v1/solutions/:email",
+  validateGetAllSolutionsAnonymous,
+  getAllSolutionsAnonymous
+);
